b4b7ed3086da51ad9927d6fe420293899620e7ff
Yuxuan Xie <yx353@cam.ac.uk>
2023-12-21 22:44:07 +0000

    edited app.py code

--- app.py
+++ app.py
@@ -35,19 +35,6 @@ def predict():
     # parse output into template
     return render_template('index_html', prediction=classification)
 
-#     image = load_img(image_path, target_size=(224, 224))
-#     image = img_to_array(image)
-#     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-#     image = preprocess_input(image)
-#     yhat = model.predict(image)
-#     label = decode_predictions(yhat)
-#     label = label[0][0]
-
-    # classification = '%s (%.2f%%)' % (label[1], label[2]*100)
-
-
-    # return render_template('index.html', prediction=classification)
-
 # run flask application, '__main__' to ensure server runs if script is executed directly, not imported  
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
